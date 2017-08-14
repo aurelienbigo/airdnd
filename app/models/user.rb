22b8541072a678b443541af6afda0fd1b5b0bb9b
Aurelien Bigo <aurelien.bigo@gmail.com>
2017-08-14 18:30:21 +0200

    add uniqueness to email

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
 
   validates :last_name, presence: true
   validates :first_name, presence: true
-  validates :email, presence: true
+  validates :email, uniqueness: true, presence: true
   validates :photo, presence: true
 
 end
