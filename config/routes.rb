44988f9cf0b06a6e1e1fdfee645901ddcc3ebe6d
aurelien bigo <aurelien.bigo@gmail.com>
2017-08-14 18:41:55 +0200

    add controller user review reservation and profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,60 @@
 Rails.application.routes.draw do
+  get 'reservations/index'
+
+  get 'reservations/show'
+
+  get 'reservations/new'
+
+  get 'reservations/create'
+
+  get 'reservations/edit'
+
+  get 'reservations/update'
+
+  get 'reservations/destroy'
+
+  get 'profiles/index'
+
+  get 'profiles/show'
+
+  get 'profiles/new'
+
+  get 'profiles/create'
+
+  get 'profiles/edit'
+
+  get 'profiles/update'
+
+  get 'profiles/destroy'
+
+  get 'reviews/index'
+
+  get 'reviews/show'
+
+  get 'reviews/new'
+
+  get 'reviews/create'
+
+  get 'reviews/edit'
+
+  get 'reviews/update'
+
+  get 'reviews/destroy'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
