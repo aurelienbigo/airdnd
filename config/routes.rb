8bd5d948cfd971c9758e09d33cf942c888ec04b6
aurelien bigo <aurelien.bigo@gmail.com>
2017-08-15 10:42:25 +0200

    routes #22

--- config/routes.rb
+++ config/routes.rb
@@ -1,59 +1,10 @@
 Rails.application.routes.draw do
-  get 'reservations/index'
 
-  get 'reservations/show'
-
-  get 'reservations/new'
-
-  get 'reservations/create'
-
-  get 'reservations/edit'
-
-  get 'reservations/update'
-
-  get 'reservations/destroy'
-
-  get 'profiles/index'
-
-  get 'profiles/show'
-
-  get 'profiles/new'
-
-  get 'profiles/create'
-
-  get 'profiles/edit'
-
-  get 'profiles/update'
-
-  get 'profiles/destroy'
-
-  get 'reviews/index'
-
-  get 'reviews/show'
-
-  get 'reviews/new'
-
-  get 'reviews/create'
-
-  get 'reviews/edit'
-
-  get 'reviews/update'
-
-  get 'reviews/destroy'
-
-  get 'users/index'
-
-  get 'users/show'
-
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/edit'
-
-  get 'users/update'
-
-  get 'users/destroy'
+  resources :profiles, except: [:destroy] do
+    resources :skill_lvls, only: [:create, :edit, :destroy]
+    resources :reviews, except: [:destroy, :edit]
+  end
+  resources :reservations
 
   devise_for :users
   root to: 'pages#home'
