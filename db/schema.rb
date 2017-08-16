a2a9256e322c1c2579694dd0429b7957ded17670
clement meyer <meyclem@gmail.com>
2017-08-16 13:53:40 +0200

    added 2 columns to reservation.rb: price and status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170815084017) do
+ActiveRecord::Schema.define(version: 20170816115158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,8 @@ ActiveRecord::Schema.define(version: 20170815084017) do
     t.bigint "profile_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
+    t.integer "price"
     t.index ["profile_id"], name: "index_reservations_on_profile_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
