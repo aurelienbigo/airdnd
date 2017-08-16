2e98661a42ad3ede4f5bfc7fbc1d4d58ed06752b
clement meyer <meyclem@gmail.com>
2017-08-16 18:05:46 +0200

    fix card display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def image_tag_for_user(user)
     if user.photo
-      cl_image_tag profile.user.photo.path, width: 100, height: 100, class: 'card-avatar'
+      cl_image_tag user.photo.path, width: 100, height: 100, class: 'card-avatar'
     else
       image_tag("http://via.placeholder.com/100x100")
     end
