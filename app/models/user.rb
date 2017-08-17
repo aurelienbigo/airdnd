c7408769170956c28171a9947b1dd68df4b4dc2c
clement meyer <meyclem@gmail.com>
2017-08-17 16:52:05 +0200

    create _tabs.scss in components #94

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # has_one :user
+  has_one :profile
   has_many :reservations
   has_many :reviews
 
